3a242b168deab9cdf791847aa4b0ef6dba2e73f3
bahaelhmimdi <elhmimdib@gmail.com>
2026-04-04 17:53:29 +0100

    Add Chrome options for headless browsing

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1280,2000")
 driver1 = webdriver.Chrome(
     service=Service(ChromeDriverManager().install()),
     options=chrome_options
