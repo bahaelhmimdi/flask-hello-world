386cca6432489f0eb13449098711f13bcdaddc3e
bahaelhmimdi <elhmimdib@gmail.com>
2026-04-06 19:19:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -333,10 +333,10 @@ def create_video1(url):
         chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--window-size=600, 1024")
+        chrome_options.add_argument("--window-size=1024, 600")
 
         driver = webdriver.Chrome(service=Service(), options=chrome_options)
-        driver.set_window_size(600, 1024)
+        driver.set_window_size(1024, 600)
 
         logging.info(f"[{task_id}] Opening page")
         driver.get(url)
